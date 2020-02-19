180d07f249154491943fa375cf9a004d039fe724
Ivan Pantic <ivan@pantas.net>
2020-02-19 01:05:38 +0100

    Add update script

---
+++ misc/update-and-push.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd )"
+
+echo "Checking out production branch"
+git checkout production
+
+echo "Getting the latest"
+git reset --hard && git pull
+
+echo "Updating data"
+npm run update-data
+
+TIMESTAMP="$(date +%Y/%m/%d %H:%M:%S)"
+echo "Creating a new commit for $TIMESTAMP"
+git add .
+git commit -m "Data update: ${TIMESTAMP}"
+
+echo "Pushing"
+git push origin production
+
+echo "Done"
